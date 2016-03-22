1c8028bf4d20542a7816ced0cc858c4ede3d885d
MostafaSoliman <mostafa.soliman.zakria@hotmail.com>
2016-03-22 14:22:08 +0200

    Wiz setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='operationLib',
+      version='0.1',
+      description='Network Security Operation Lib, parse CISCO FIREWALL configuration into opbjects',
+      url='https://github.com/MostafaSoliman/operationLib',
+      author='Mostafa Soliman',
+      author_email='mostafa.soliman.zakria@hotmail.com',
+      license='MIT',
+      packages=['operationLib'],
+      zip_safe=False)
